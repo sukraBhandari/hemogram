a650ee34cf92b3493ee70be582bb2d1f9e4fc13b
sukrab <sukrab@yahoo.com>
2019-04-07 11:56:13 -0700

    updated s3 config and sample view page

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ class Config:
 
     # files storage config
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY_ID = os.environ.get('AWS_SECRET_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_FILE_OVERWRITE = False
     AWS_DEFAULT_ACL = None
